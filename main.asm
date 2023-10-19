750a9bdd139c1c428b3188c22f6ef2ba61f15d02
Ivanor-dev <i279746@gmail.com>
2023-10-19 19:55:31 -0300

    Só pra ter o primeiro arquivo

---
+++ main.asm
@@ -0,0 +1,11 @@
+.686
+
+.model flat, stdcall
+option casemap: none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc ; nessa lib tem a leitura de arquivos 
+include \masm32\include\msvcrt.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\msvcrt.lib
+include \masm32\macros\macros.asm
