5ea27fda9f8783f7f1b51b5bc3af4e637f24fa9a
Guilherme Dantas <103064370+guidantas21@users.noreply.github.com>
2023-10-23 15:57:48 -0300

    feat: read file name

--- main.asm
+++ main.asm
@@ -7,63 +7,95 @@ include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc ; nessa lib tem a leitura de arquivos 
 include \masm32\include\masm32.inc
 include \masm32\include\msvcrt.inc
+
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\msvcrt.lib
 includelib \masm32\lib\masm32.lib
+
 include \masm32\macros\macros.asm
 
 
 .data
-    file db "fotoanonima.bmp", 0
-    fileSizeMessage db "Tamanho do arquivo: %d bytes", 0
-    
-    
-    prompt db "Digitar nome do arquivo: ", 0
-    outputHandle dd 0
-    output dd 0
-    write_count dd 0
+    inputFileName db "Input file name (.bmp): ", 0H
 
-.data?
-    hStdOut HANDLE ?
+    fileName db 50 dup(0) ; 100 bytes = 800 bits
+    fileNameLength dd 0
+
+    outputHandle dd 0
+    inputHandle dd 0
+
+    consoleCount dd 0
+
+    output dd 0
 
 .code
 
 
     start:
-    ; mov esi, offset file ; Armazenar apontador da string em esi
-    ;proximo:
-     ;   mov al, [esi] ; Mover caractere atual para al
-      ;  inc esi ; Apontar para o proximo caractere
-       ; cmp al, 13 ; Verificar se eh o caractere ASCII CR - FINALIZAR
-        ;jne proximo
-        ;dec esi ; Apontar para caractere anterior
-        ;xor al, al ; ASCII 0
-        ;mov [esi], al ; Inserir ASCII 0 no lugar do ASCII CR
+        ; -- get input/output handle
+        push STD_INPUT_HANDLE
+        call GetStdHandle
+        mov inputHandle, eax
 
-        invoke GetStdHandle, STD_OUTPUT_HANDLE
+        push STD_OUTPUT_HANDLE
+        call GetStdHandle
         mov outputHandle, eax
-        ;invoke CreateFile, addr file, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
+
+        ; write input message (file name)
+        push NULL
+        push offset consoleCount
+        push sizeof inputFileName
+        push offset inputFileName
+        push outputHandle
+        call WriteConsole
+
+        ; read file name
+        push NULL
+        push offset consoleCount
+        push sizeof fileName
+        push offset fileName
+        push inputHandle
+        call ReadConsole
+
+        ; get file name string length
+        push offset fileName
+        call StrLen
+        mov fileNameLength, eax
+
+        mov esi, offset fileName ; Armazenar apontador da string em esi
+
+        next:
+            mov al, BYTE PTR [esi] ; Mover caractere atual para al
+            inc esi ; Apontar para o proximo caractere
+            cmp al, 13 ; Verificar se eh o caractere ASCII CR - FINALIZAR
+            jne next
+            
+        dec esi ; Apontar para caractere anterior
+        xor al, al ; ASCII 0
+        mov BYTE PTR [esi], al ; Inserir ASCII 0 no lugar do ASCII CR
+
         push 0
         push FILE_ATTRIBUTE_NORMAL
         push OPEN_EXISTING
         push NULL
         push 0
         push GENERIC_READ
-        push offset file
+        push offset fileName
         call CreateFile
 
         invoke GetFileSize, eax, 0 
-        ;cmp eax, 0
-        ;jl dimensionerSizeOfArq
-        ;jmp finishPart
+        ; cmp eax, 0
+        ; jl dimensionerSizeOfArq
+        ; jmp finish
 
         dimensionerSizeOfArq:
             push output
             push eax
             call dwtoa
+            
             ;invoke WriteConsole, outputHandle, addr eax, sizeof eax, addr write_count, NULL
             push NULL
-            push offset write_count
+            push offset consoleCount
             push sizeof output
             push offset output
             push outputHandle
@@ -73,8 +105,7 @@ include \masm32\macros\macros.asm
             ;push eax
             ;call CloseHandle
 
-        finishPart:
+        finish:
             invoke ExitProcess, 0
-
 
     end start
